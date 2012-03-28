8a7111842b498ed1e69ccfddec056ed48d7a451b
mikhail lozanov <mlozanov@gmail.com>
2012-03-28 23:01:51 +0300

    native scheme and extempore language versions of osc receiver callbacks native scheme will be better because easier string manipulation. then extempore lang function will store properly the information

--- oscwrapper.scm
+++ oscwrapper.scm
@@ -1,43 +1,88 @@
 
 ;;; start osc receiver
-(bind-val _fader1 double 0.0)
-(bind-val _fader2 double 0.0)
-(bind-val _fader3 double 0.0)
-(bind-val _fader4 double 0.0)
-(bind-val _fader5 double 0.0)
+(bind-val _fader11 double 0.0)
+(bind-val _fader12 double 0.0)
+(bind-val _fader13 double 0.0)
+(bind-val _fader14 double 0.0)
+(bind-val _fader15 double 0.0)
 
-(bind-val _toggle1 double 0.0)
-(bind-val _toggle2 double 0.0)
-(bind-val _toggle3 double 0.0)
-(bind-val _toggle4 double 0.0)
+(bind-val _toggle11 double 0.0)
+(bind-val _toggle12 double 0.0)
+(bind-val _toggle13 double 0.0)
+(bind-val _toggle14 double 0.0)
+
+(bind-val _xy |2,double| 0.0)
+
+(bind-val _toggle31 double 0.0)
+(bind-val _toggle32 double 0.0)
+(bind-val _toggle33 double 0.0)
+(bind-val _toggle34 double 0.0)
+
+(bind-val _push |16,double| 0.0)
 
 (definec osc-receive-8000
   (lambda (address:i8* types:i8* args:i8*)
     (let ((data (bitcast args i32*)))
-      (printf "address:%s  type:%s arg1:%f arg2:%lld\n"
-	      address types
-	      (ftod (unswap32f (pref data 0)))
-	      (unswap32i (pref data 1)))
-	  (cond ((= (strcmp address "/1/fader1") 0)
-			 (set! _fader1 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/fader2") 0)
-			 (set! _fader2 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/fader3") 0)
-			 (set! _fader3 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/fader4") 0)
-			 (set! _fader4 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/fader5") 0)
-			 (set! _fader5 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/toggle1") 0)
-			 (set! _fader4 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/toggle2") 0)
-			 (set! _fader4 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/toggle3") 0)
-			 (set! _fader4 (ftod (unswap32f (pref data 0)))))
-			((= (strcmp address "/1/toggle4") 0)
-			 (set! _fader4 (ftod (unswap32f (pref data 0)))))
-			)
-	  (printf "+"))))
+	  (let ((a (ftod (unswap32f (pref data 0))))
+			(b (ftod (unswap32f (pref data 1))))
+			(i 0))
+		(printf "address:%s  type:%s arg1:%f arg2:%f\n"
+				address types a b)
+		(cond ((= (strcmp address "/1/fader1") 0)
+			   (set! _fader11 a))
+			  ((= (strcmp address "/1/fader2") 0)
+			   (set! _fader12 a))
+			  ((= (strcmp address "/1/fader3") 0)
+			   (set! _fader13 a))
+			  ((= (strcmp address "/1/fader4") 0)
+			   (set! _fader14 a))
+			  ((= (strcmp address "/1/fader5") 0)
+			   (set! _fader15 a))
+			  ((= (strcmp address "/1/toggle1") 0)
+			   (set! _toggle11 a))
+			  ((= (strcmp address "/1/toggle2") 0)
+			   (set! _toggle12 a))
+			  ((= (strcmp address "/1/toggle3") 0)
+			   (set! _toggle13 a))
+			  ((= (strcmp address "/1/toggle4") 0)
+			   (set! _toggle14 a))
+			  ((= (strcmp address "/3/toggle1") 0)
+			   (set! _toggle31 a))
+			  ((= (strcmp address "/3/toggle2") 0)
+			   (set! _toggle32 a))
+			  ((= (strcmp address "/3/toggle3") 0)
+			   (set! _toggle33 a))
+			  ((= (strcmp address "/3/toggle4") 0)
+			   (set! _toggle34 a))
+			  ((= (strcmp address "/3/xy") 0)
+			   (aset! _xy 0 a)
+			   (aset! _xy 1 b))))
+		)
+	  (printf "+")))
+;		(dotimes (i 16)
+;		  (if (= (strcmp address (strcat "/2/push" (symbol->string i))) 0)
+;			  (aset! _push i a))))
 
 (io:osc:start-server 8000 "osc-receive-8000" (llvm:get-native-function "osc-receive-8000"))
-;;; osc portion ends
+
+
+;; native scheme
+
+(define *push* ())
+
+(define osc-receive-8000
+  (lambda (timestamp address . args)
+	(println 'port 8000 address '-> args)
+
+	;; analyze push buttons
+	(dotimes (i 17)
+	  (if (string=? (string-append "/2/push" (number->string i)) address)
+		  (println i)))
+
+	;; ...
+
+	(println '+)))
+
+(io:osc:start-server 8000 "osc-receive-8000")
+
+
